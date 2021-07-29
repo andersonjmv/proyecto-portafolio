6d8bb7f3519d0c9693b37ee84b597b68b3358a9c
andersonjmv <anderson.manosalva@gmail.com>
2021-07-29 18:14:50 -0500

    Componentes Estudio y Experiencia

---
+++ src/components/Estudios.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import {Title, SubTitle} from '../style/styled';
+
+
+export default class Estudios extends Component {
+    render() {
+        return (
+            <div className="Estudios-container">
+                <Title>Mis Estudios</Title>
+                <SubTitle>Desarrollador Frontend</SubTitle>
+                <p>Academia Geek - En progreso</p>                        
+            </div>
+        )
+    }
+}
---
+++ src/components/Experiencia.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import {Title, SubTitle} from '../style/styled';
+
+
+export default class Experiencia extends Component {
+    render() {
+        return (
+                <div className="Container-experiencia">
+                    <div className="Experiencia-item">
+                        <Title>Experiencia</Title>
+                        <SubTitle> Desarrollador Frontend Junior</SubTitle>
+                        <p>Academia Geek - Actual</p>
+                    </div>
+                </div>                
+        )
+    }
+}
